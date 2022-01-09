6178ec4929e735750b19b9c7950b0cf21e2f8b58
Evoke0118 <evoke.baddvoltage@gmail.com>
2022-01-09 18:02:53 +0900

    編集、削除機能実装

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
 
@@ -23,6 +24,8 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::post('news/create', 'Admin\NewsController@create');
     Route::get('news', 'Admin\NewsController@index');
     Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
  });
 //Route::get('admin/news/create', 'Admin\NewsController@add'); この書き方でもおっけい
 // グループ化することで複数行うことができて便利でわかりやすい
@@ -31,10 +34,10 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
        //ProfileController
        Route::get('profile/create', 'Admin\ProfileController@add');
        Route::post('profile/create', 'Admin\ProfileController@create');
-       Route::get('profile/index', 'Admin\ProfileController@index');
+       Route::get('profile', 'Admin\ProfileController@index');
        Route::get('profile/edit', 'Admin\ProfileController@edit');
        Route::post('profile/edit', 'Admin\ProfileController@update');
-});
+       Route::get('profile/delete', 'Admin\ProfileController@delete'); });
 // NewsControllerのgroupの中に入れることはできるのか？
 
 
